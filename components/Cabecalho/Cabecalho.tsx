fca5ae06ccdba42bf1fd76dfff06a5acf75c5ce1
João Melo <130319742+joaomelo90@users.noreply.github.com>
2024-12-12 21:58:03 -0300

    primeiro commit

---
+++ components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,19 @@
+const Cabecalho = () => {
+    return(
+        <body className=" w-screen h-full flex flex-col items-center justify-center">
+        <main
+        className=" w-64 mt-10">
+            <div >
+                <img 
+                src="/imagens/hireMe.jpg"
+                alt="hireMe"
+                className=" w-full"                
+                />
+            </div>
+            <p className=" text-center">Encontre o Profissional para o seu Trabalho!</p>
+        </main>
+        </body>
+    )
+}
+
+export default Cabecalho
---
+++ components/themes/theme.ts
@@ -0,0 +1,44 @@
+import { createTheme } from "@mui/material";
+
+const tema = createTheme({
+  palette: {
+    primary: {
+      main: "#2596be",
+    },
+    secondary: {
+      main: "#F5F5F5",
+    },
+    text: {
+      primary: "#2596be",
+      secondary: "#6c6767",
+    },
+  },
+  components:{
+    MuiButton:{
+        styleOverrides:{
+            root:{
+                color: '#fff',
+                borderRadius:0,
+                backgroundColor: "#2596be",
+                textTransform: 'none',
+                fontWeight:'bold',
+                paddingTop: '12px',
+                paddingBottom: '12px',
+                ":hover":{
+                    backgroundColor: "#2596be",
+                }
+            }
+        }
+    },
+    MuiTextField:{
+        defaultProps:{
+            InputLabelProps:{
+                required: false,
+            },
+            required: true,
+        }
+    }
+  }
+});
+
+export default tema
