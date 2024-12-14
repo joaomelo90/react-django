3024368d030061278ef9768a80cbe4154cc3d47a
João Melo <130319742+joaomelo90@users.noreply.github.com>
2024-12-13 22:31:06 -0300

    ajuste tela de trabalho

--- components/Cabecalho/Cabecalho.tsx
+++ components/Cabecalho/Cabecalho.tsx
@@ -1,14 +1,14 @@
 
 const Cabecalho = () => {
     return(
-        <div className=" w-screen h-full flex flex-col items-center justify-center">
+        <div className=" w-full h-full flex flex-col items-center justify-center">
         <main
         className=" w-64 mt-10 ">
             <div >
                 <img 
                 src="/imagens/hireMe.jpg"
                 alt="hireMe"
-                className=" w-full"                
+                className=" w-full rounded-xl opacity-75"                
                 />
             </div>
             <p className=" text-center text-primary">Encontre o Profissional para o seu Trabalho!</p>
--- components/Lista/Lista.tsx
+++ components/Lista/Lista.tsx
@@ -1,16 +1,34 @@
 const Lista = ()=>{
     return (
-        <div className="listaStyled" >
-            <div className="itemLista">
-                <img src="https://github.com/joaomelo90.png"/>
+        <ul className="listaStyled w-full w-max m-[0_auto] p-[10px_2px_10px_2px] grid grid-cols-1 lg:grid-cols-2 gap-14 " >
+            <li className="itemLista list-none ">
+                <img src="https://github.com/joaomelo90.png"
+                className=" md:w-auto w-96 rounded-3xl"
+                />
                 <div className="nomeDescricao">
-                    <div className="nome">João Melo</div>
-                    <div className="valor">R$ 80 por hora</div>
-                    <div className="descricao">Trabalha com Tecnologias Web</div>
+                    <div className="nome mt-4 font-bold">João Melo</div>
+                    <div className="valor m-0 font-bold text-primary">R$ 80 por hora</div>
+                    <div className="descricao break-words ">Trabalha com Tecnologias Web</div>
                 </div>
-            </div>
+            </li>
 
-        </div>
+
+
+            <li className="itemLista list-none p-1">
+                <img src="https://github.com/joaomelo90.png"
+                className=" md:w-auto w-96 rounded-3xl "
+                />
+                <div className="nomeDescricao">
+                    <div className="nome mt-4 font-bold">João Melo</div>
+                    <div className="valor m-0 font-bold text-primary">R$ 80 por hora</div>
+                    <div className="descricao break-words ">Trabalha com Tecnologias Web</div>
+                </div>
+            </li>
+
+        </ul>
+
+
+        
     )
 }
 
